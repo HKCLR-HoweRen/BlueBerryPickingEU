54062385dc570a97071fe1d6ae45cdd4ca67933c
Howe <hren@hkclr.hk>
2025-08-27 16:07:34 +0800

    Update test_vice_arm.py
    change to rm63 robot and class RobotArmController, the dump pose and undertake pose is waiting change

--- test_vice_arm.py
+++ test_vice_arm.py
@@ -1,39 +1,83 @@
-from eu_arm.robot_arm_interface import RobotArm
+
+#import numpy as np
+import threading, time
+import cv2
+import sys, os
+import argparse, json
+
+# Add the parent directory of src to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from rm_arm.core.rm_robot_controller import RobotArmController
 from utils.data_io import DataRecorder
 
+#from env_cfg import *
 
-def init()
+# Realman robot version grasp motion
+
+def init_vice_arm(use_real_robot=False):
+    robot = RobotArmController("192.168.1.102", 8080, 3)
+    work_frmae_ret = robot.robot.rm_change_work_frame("Base")
+    robot.get_arm_software_info()
+    return 0
+
+
+
+def goto_undertake_pose():
+    #waiting for change
+    q_undertake_pose=[95.2170, 57.6819, -113.0309, -9.6590, -63.36500, 176.8399]
+    robot.moveJ(q_undertake_pose)
+    return 0
+
+
+def execute_dump_action():
+    #waiting for change
+    q_sump_pose=[95.2170, 57.6819, -113.0309, -9.6590, -63.36500, 176.8399]
+    robot.moveJ(q_dump_pose)
+    return 0
     
 
 if __name__ == "__main__":  
-     robot, pump_ctrl, rs_ctrl, grasp_algo = init_all()
+    init_ret = init_vice_arm()
 
-    # move to initial pose            
-    q_init = sensing_pose_list[sensing_pose_idx]
-    robot.moveJ(q_init)
-
-    # Visualization
-    cv2.namedWindow('test_grasping', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('test_grasping', int(1280), int(720))
-    cv2.setMouseCallback("test_grasping", click_callback)
+    # move to initial pose   
+    # need change to new pose
+    q_init = [95.2170, 57.6819, -113.0309, -9.6590, -63.36500, 176.8399]#sensing pose 
+    if init_ret==0:
+        robot.moveJ(q_init)
 
     ## ================= Global Variables =================
-    # EU arm control stuff
+    # RML63 vice-arm control stuff
     q_curr, _ = robot.get_current_state()
-    j_idx = 0
     JMOVE_STEP = 0.005
     q_target = q_init
-    PRE_GRASP_DIST = 0.05 - 0.005 # mm
-    GRIPPER_LENGTH = 0.135 # mm
-    GRASP_OFFSET = 0.015  
-    saving_data_idx = 0
-    refPt = (int(1280/2), int(720/2)) # center of img
-    refPt_updated = False
     marching_dist = 0.
     JLIMIT = 3.1
+
     ## ================= Global Variables =================
 
-    data_io = DataRecorder('tmp/picking_data')
 
     try:
         while True:
+            #
+
+            if key & 0xFF == ord('q') or key == 27: # Press esc or 'q' to close the image window
+                print("\n===== quit =====")  
+                break
+
+            elif key & 0xFF == ord('u'):
+                goto_undertake_pose()
+                print("\n=goto undetake=")
+                continue
+
+            elif key & 0xFF == ord('d'):
+                execute_dump_action()
+                print("\n=goto dump=")
+                continue
+
+
+      
+    finally:
+        if robot:
+            robot.disconnect()  
+            print("Program terminated.")
